bda7e27402323b93435325f74082a486678470ea
brolive <brolive@my.lonestar.edu>
2019-10-04 11:48:55 -0500

    First

---
+++ AI_Test/Assets/AiTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiTest : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+
+    public float speed = 5;
+    public float watchDistance = 5.0f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+        //if (Vector3.Distance(transform.position, target.position) < watchDistance)
+        //transform.LookAt(target);
+        //transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+            
+    }
+}
---
+++ AI_Test/Assets/PatrolBot.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBot : MonoBehaviour
+{
+    public List<Transform> nodes;
+    public int nodeIndex = 0;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+
+    public enum States
+    {
+        patrol,
+        idle,
+        charge
+    }
+
+    public States state = States.idle;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case States.idle:
+                UpdateIdle();
+                break;
+
+            case States.patrol:
+                UpdatePatrol();
+                break;
+        }
+    }
+
+    void UpdateIdle()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            state = States.patrol;
+        }
+    }
+
+    void UpdatePatrol()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+                                          nodes[nodeIndex].transform.position,
+                                          speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, nodes[nodeIndex].transform.position) < 2)
+        {
+            nodeIndex++;
+
+            if (nodeIndex >= nodes.Count)
+            {
+                nodeIndex = 0;
+            }
+        }
+    }
+}
